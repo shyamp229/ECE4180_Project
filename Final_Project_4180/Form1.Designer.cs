8394a57b7bae478d7d8600bbc4e139895c82b542
shyam <shyamp229@gmail.com>
2020-12-02 20:20:23 -0500

    serialPort added/writes to mbed

--- Final_Project_4180/Form1.Designer.cs
+++ Final_Project_4180/Form1.Designer.cs
@@ -28,86 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(106, 29);
+            this.comboBox1.Location = new System.Drawing.Point(91, 25);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.Size = new System.Drawing.Size(104, 21);
             this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 32);
+            this.label1.Location = new System.Drawing.Point(21, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "COM PORT";
             // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(106, 188);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(110, 45);
-            this.trackBar1.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 197);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Volume";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(300, 356);
+            this.button1.Location = new System.Drawing.Point(24, 309);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(204, 53);
+            this.button1.Size = new System.Drawing.Size(629, 46);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Play/Pause";
+            this.button1.Text = "Play/Stop";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(53, 356);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(204, 53);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Previous";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(558, 356);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(204, 53);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Next";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(319, 263);
+            this.label3.Location = new System.Drawing.Point(264, 255);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(169, 15);
+            this.label3.Size = new System.Drawing.Size(148, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Name of currrent song playing";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -115,38 +80,43 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(579, 24);
+            this.comboBox2.Items.AddRange(new object[] {
+            "Africa",
+            "Around the World",
+            "Beautiful Life",
+            "Don\'t Speak",
+            "My Love",
+            "Song1_test"});
+            this.comboBox2.Location = new System.Drawing.Point(496, 21);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(183, 23);
+            this.comboBox2.Size = new System.Drawing.Size(157, 21);
             this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedValueChanged += new System.EventHandler(this.comboBox2_SelectedValueChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(525, 29);
+            this.label4.Location = new System.Drawing.Point(450, 25);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Song";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 450);
+            this.ClientSize = new System.Drawing.Size(691, 390);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Music Controller";
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,14 +126,11 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- Final_Project_4180/Form1.cs
+++ Final_Project_4180/Form1.cs
@@ -7,18 +7,161 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
 
 namespace Final_Project_4180
 {
     public partial class Form1 : Form
     {
+        private String[] portNames;
+        private String[] songs = { "africa-toto", "around_the_world-atc", "beautiful_life-ace_of_base", "dont_speak-no_doubt", "my-love", "Song1_test" };
+        private bool play = false;
+        private String song = "";
+      
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
         {
+            serialPort1.PortName = comboBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // add port names to comboBox1
+
+            Debug.Print("hello 4180 project!");
+            try
+            {
+                Debug.Print("init load");
+                portNames = SerialPort.GetPortNames();
+                comboBox1.Items.AddRange(portNames);
+                comboBox1.SelectedIndex = 0;
+                Debug.Print("end load");
+            }
+            catch (Exception err)
+            {
+                Debug.Print("loading error: " + err.Message);
+            }
+            Debug.Print("Connected Mbed to " + comboBox1.Text);
+
+
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.Open();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+
+        /* Songs:
+            Africa
+            Around the World
+            Beautiful Life
+            Don't Speak
+            My Love
+            Song1_test 
+        */
+        /*
+         * info:
+         * button1 = play/pause
+         * 1 = Africa
+         * 2 = Around the World
+         * 3 = Beautiful Life
+         * 4 = Don't Speak
+         * 5 = My Love
+         * 6 = Song1_test
+         */
+        private void comboBox2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedItem.ToString())
+            {
+                case "Africa":
+                    // send song string to mbed
+                    Debug.Print("africa-toto");
+                    song = "!1";
+                    break;
+                case "Around the World":
+                    Debug.Print("around_the_world-atc");
+                    song = "!2";
+                    break;
+                case "Beautiful Life":
+                    Debug.Print("beautiful_life-ace_of_base");
+                    song = "!3";
+                    break;
+                case "Don't Speak":
+                    Debug.Print("dont_speak-no_doubt");
+                    song = "!4";
+                    break;
+                case "My Love":
+                    Debug.Print("my-love");
+                    song = "!5";
+                    break;
+                case "Song1_test":
+                    Debug.Print("Song1_test");
+                    song = "!6";
+                    break;
+                default:
+                    break;
+            }
+            label3.Text = comboBox2.SelectedItem.ToString();
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(song);
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Open();
+                } catch (Exception err)
+                {
+                    Debug.Print("Error: " + err.Message);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             * if (play == true)
+             *      send signal via serialPort to play song
+             *      
+             * if (play == false)
+             *      send signal via serialPort to stop song
+             */
+            play = !play;
+            if (serialPort1.IsOpen)
+            {
+                if (play == true)
+                {
+                    serialPort1.Write("!P");
+                }
+                else
+                {
+                    serialPort1.Write("!S");
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Open();
+                } catch (Exception err)
+                {
+                    Debug.Print("Error: " + err.Message);
+                }
+            }
 
         }
     }
